f8ebdfbfeb64953e9ae4e57d6468a7dc090a47d8
Agatha North <rouborn@gmail.com>
2020-07-31 13:43:44 -0700

    tweaks

--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,7 @@ process.env.JWT_EXPIRY = '3m';
 
 require('dotenv').config();
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
   || "postgresql://devwi@localhost/qapla-test"
 
 const { expect } = require('chai');
